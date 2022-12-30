a08d13a7acf980763adb3b64966c7dc1a67e79d7
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-12-30 21:56:31 +0530

    weather app running

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "74605ce7f9msh514377dd42cc461p1da6d7jsna6f93faf88df", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "65018afece473ef6e2e8ee0d7dbf73a9"; // enter your key from openweather API
